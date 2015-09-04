fbe96bc76d7b20b4db3a9af470f0705106617517
Malte <malauter@yahoo.de>
2015-09-04 13:35:22 +0200

    TextBox für int-Werte erstellt

---
+++ SportsFestivalManager.Wpf/IntBox.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SportsFestivalManager.Wpf
+{
+    public class IntBox : TextBox
+    {
+        protected override void OnPreviewTextInput(TextCompositionEventArgs e)
+        {
+            int dummy;
+            if (!int.TryParse(e.Text, out dummy))
+                e.Handled = true;
+        }
+    }
+}
